f28549750cbcd422d57dd1b124e47597f70887d9
AnastasyKip <1746237@gmail.com>
2025-06-27 14:40:22 +0100

    fixed last

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,27 +1,28 @@
-import runGame from '../index.js'
-import getRandomNumber from '../utils/random.js'
+import runGame from "../index.js";
+import getRandomNumber from "../utils/random.js";
 
-const description = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+const description =
+  'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (n) => {
   if (n <= 1) {
-    return false
+    return false;
   }
   for (let i = 2; i < n; i++) {
     if (n % i === 0) {
-      return false
+      return false;
     }
   }
-  return true
-}
+  return true;
+};
 
 const makeRound = () => {
-  const number = getRandomNumber(1, 100)
-  const question = `${number}`
-  const answer = isPrime(number) ? 'yes' : 'no'
-  return { question, answer }
-}
+  const number = getRandomNumber(1, 100);
+  const question = `${number}`;
+  const answer = isPrime(number) ? "yes" : "no";
+  return { question, answer };
+};
 
 export default function startPrimeGame() {
-  runGame(description, makeRound)
+  runGame(description, makeRound);
 }
